348c2554d4e214297719cbe0f97cae58651a6b45
Juan Jo Ruiz Ferrer <jjgeek.sis@gmail.com>
2017-08-12 20:29:18 -0400

    gem para ckeditor, vistas de posts y rutas para acceso

--- Gemfile
+++ Gemfile
@@ -13,6 +13,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'simple_form', '~> 3.2'
 gem 'bootstrap-sass', '~> 3.3'
+gem 'ckeditor', '~> 4.1'
 
 group :development, :test do
   gem 'byebug'
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,19 @@
 
 ActiveRecord::Schema.define(version: 20170812235443) do
 
+  create_table "ckeditor_assets", force: :cascade do |t|
+    t.string   "data_file_name",               null: false
+    t.string   "data_content_type"
+    t.integer  "data_file_size"
+    t.string   "type",              limit: 30
+    t.integer  "width"
+    t.integer  "height"
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+  end
+
+  add_index "ckeditor_assets", ["type"], name: "index_ckeditor_assets_on_type"
+
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
